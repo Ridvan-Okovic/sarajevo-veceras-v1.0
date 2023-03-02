c47c7fb578e540c70ae8a74fac67d767cbb37da6
Ridvan-Okovic <okovic.ridvan@gmail.com>
2023-03-02 15:10:56 +0100

    Styling changes

--- src/components/Event/Event.js
+++ src/components/Event/Event.js
@@ -5,7 +5,7 @@ import { MdCelebration } from 'react-icons/md';
 
 const Event = (props) => {
   return (
-    <div className="w-[20rem] font-montserrat shadow-[0px_10px_15px_rgba(0,0,0,0.2)] rounded-xl hover:scale-[1.02] transition-all duration-300">
+    <div className="w-[20rem] font-montserrat shadow-[0px_10px_15px_rgba(0,0,0,0.2)] rounded-xl hover:scale-[1.02] transition-all duration-300 bg-[#FAF8FF]">
       <div className="w-full h-[213.5px]">
         <img
           className="rounded-t-lg object-contain"
@@ -14,30 +14,30 @@ const Event = (props) => {
         />
       </div>
 
-      <h2 className="text-center mx-[10%] text-2xl mt-4 mb-2 font-bold">
+      <h2 className="text-center mx-[10%] text-2xl mt-4 mb-2 font-semibold text-[#000]">
         {props.name}
       </h2>
 
       <div className="flex flex-col mx-[10%] text-lg font-light">
-        <h3 className="flex flex-row items-center mb-2 border-b-[1px] border-gray-300">
-          <MdCelebration className="text-[#e14658] mr-2" />
+        <h3 className="flex flex-row items-center mb-2 border-b-[1px] text-black border-gray-300">
+          <MdCelebration className="text-[#ACA7CB] mr-2" />
           {props.opis}
         </h3>
-        <h3 className="flex flex-row items-center mb-2 border-b-[1px] border-gray-300">
-          <FaMapPin className="text-[#e14658] mr-2" />
+        <h3 className="flex flex-row items-center mb-2 border-b-[1px] text-black border-gray-300">
+          <FaMapPin className="text-[#ACA7CB] mr-2" />
           {props.address}
         </h3>
-        <h3 className="flex flex-row items-center mb-2 border-b-[1px] border-gray-300">
-          <FaClock className="text-[#e14658] mr-2" />
+        <h3 className="flex flex-row items-center mb-2 border-b-[1px] text-black border-gray-300">
+          <FaClock className="text-[#ACA7CB] mr-2" />
           {props.time}
         </h3>
-        <h3 className="flex flex-row items-center border-b-[1px] border-gray-300">
-          <FaCalendar className="text-[#e14658] mr-2" />
+        <h3 className="flex flex-row items-center border-b-[1px] text-black border-gray-300">
+          <FaCalendar className="text-[#ACA7CB] mr-2" />
           <EventDate datum={props.date} />
         </h3>
       </div>
       <div className="px-[10%] flex justify-center">
-        <button className="flex text-[#eee] justify-center shadow-md items-center w-full h-[2.5rem] bg-[#e14658] my-4 rounded-full text-lg hover:text-black hover:text-xl transition-all duration-300">
+        <button className="flex text-[#fff] justify-center shadow-md items-center w-full h-[2.5rem] bg-[#ACA7CB] my-4 rounded-full text-lg hover:text-[#474554] hover:text-xl transition-all duration-300">
           <FaHeart className="" />
         </button>
       </div>
--- src/components/Layout/NavBar.js
+++ src/components/Layout/NavBar.js
@@ -17,30 +17,30 @@ const NavBar = (props) => {
   };
 
   return (
-    <nav className="sticky top-0 z-10 w-full h-[5rem] px-[10%] flex flex-row items-center justify-between bg-[#22252c]">
-      <h2 className="text-2xl font-semibold text-[#e14658]">Logo</h2>
-      <div className="flex gap-10 text-xl text-[#e14658] items-baseline">
+    <nav className="sticky top-0 z-10 w-full h-[5rem] px-[10%] flex flex-row items-center justify-between bg-[#585BE9] text-white uppercase">
+      <h2 className="text-2xl font-semibold">Logo</h2>
+      <div className="flex gap-10 text-lg items-baseline">
         {/* //TODO: Change a tags to be Links using React Router! */}
         <a href="#">Home</a>
         <a href="#">Liked</a>
         <button
           onClick={openFilterHandler}
-          className="text-md border-2 border-[#e14658] text-[#e14658] py-1 px-6 hover:border-[#eee] hover:text-[#eee] active:border-[#eee] active:text-[#eee] transition-all duration-300"
+          className="border-2 border-white text-white uppercase py-1 px-6 hover:border-[#eee] hover:text-[#eee] active:border-[#eee] active:text-[#eee] transition-all duration-300"
         >
           Filter
         </button>
       </div>
-      <div className="flex justify-end">
+      <div className="flex items-center justify-center">
         {isSearchOpen && (
           <input
             onChange={searchChangeHandler}
-            className="outline-none bg-transparent border-b-[3px] placeholder:text-gray-300 text-white border-[#e14658] mr-4 transition-all"
+            className="outline-none bg-transparent border-b-[2px] placeholder:text-gray-300 text-white border-white mr-4"
             placeholder="Sloga..."
           />
         )}
         <FaSearch
           onClick={searchBarHandler}
-          className="text-xl text-[#e14658] cursor-pointer w-[54.86px]"
+          className="text-xl text-white cursor-pointer w-[54.86px]"
         />
       </div>
     </nav>
