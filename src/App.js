30faadf8156ea83539b4900acedc1f66a413a401
Ridvan-Okovic <okovic.ridvan@gmail.com>
2023-03-11 20:44:17 +0100

    Added routing between home and liked pages

--- src/App.js
+++ src/App.js
@@ -1,83 +1,30 @@
-import { useState, useEffect } from 'react';
-import EventContainer from './components/Event/EventContainer';
-import Filter from './components/Layout/Filter';
-import NavBar from './components/Layout/NavBar';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import EventProvider from './components/context/EventProvider';
 
+import HomePage from './components/Pages/HomePage';
+import LikedEvents from './components/Pages/LikedEvents';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <EventProvider>
+        <HomePage />
+      </EventProvider>
+    ),
+  },
+  {
+    path: '/liked',
+    element: (
+      <EventProvider>
+        <LikedEvents />
+      </EventProvider>
+    ),
+  },
+]);
+
 const App = () => {
-  const [searchValue, setSearchValue] = useState('');
-  const [isFilterOpened, setIsFilterOpened] = useState(false);
-
-  // * Checkbox logic for club
-  const [isClubChecked, setIsClubChecked] = useState(false);
-  const [checkedClubValue, setCheckedClubValue] = useState('');
-
-  // * Checkbox logic for pub
-  const [isPubChecked, setIsPubChecked] = useState(false);
-  const [checkedPubValue, setCheckedPubValue] = useState('');
-
-  // * Checkbox logic for club
-  const [isOpenChecked, setIsOpenChecked] = useState(false);
-  const [checkedOpenValue, setCheckedOpenValue] = useState('');
-
-  // * Reset checkbox logic
-  const resetClubCheckboxHandler = (isChecked) => {
-    if (isChecked === false) {
-      setCheckedClubValue('');
-    }
-  };
-
-  const resetPubCheckboxHandler = (isChecked) => {
-    if (isChecked === false) {
-      setCheckedPubValue('');
-    }
-  };
-
-  const resetOpenCheckboxHandler = (isChecked) => {
-    if (isChecked === false) {
-      setCheckedOpenValue('');
-    }
-  };
-
-  // * Resetting the checkbox name value after it has been de-selected
-  useEffect(() => {
-    resetClubCheckboxHandler(isClubChecked);
-    resetOpenCheckboxHandler(isOpenChecked);
-    resetPubCheckboxHandler(isPubChecked);
-  }, [isClubChecked, isPubChecked, isOpenChecked]);
-
-  return (
-    <EventProvider>
-      <NavBar
-        onAddSearchTermHandler={setSearchValue}
-        setIsFilterOpened={setIsFilterOpened}
-      />
-      {isFilterOpened && (
-        <Filter
-          setIsPubChecked={setIsPubChecked}
-          setCheckedPubValue={setCheckedPubValue}
-          // ----------------------------------- //
-          setIsClubChecked={setIsClubChecked}
-          setCheckedClubValue={setCheckedClubValue}
-          // ----------------------------------- //
-          setIsOpenChecked={setIsOpenChecked}
-          setCheckedOpenValue={setCheckedOpenValue}
-        />
-      )}
-      <EventContainer
-        searchValue={searchValue}
-        // ------------------------------------ //
-        isClubChecked={isClubChecked}
-        checkedClubValue={checkedClubValue}
-        // ------------------------------------ //
-        isPubChecked={isPubChecked}
-        checkedPubValue={checkedPubValue}
-        // ------------------------------------ //
-        isOpenChecked={isOpenChecked}
-        checkedOpenValue={checkedOpenValue}
-      />
-    </EventProvider>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
---
+++ src/components/Pages/HomePage.js
@@ -0,0 +1,84 @@
+import { useState, useEffect } from 'react';
+
+import NavBar from '../Layout/NavBar';
+import Filter from '../Layout/Filter';
+import EventContainer from '../Event/EventContainer';
+import EventProvider from '../context/EventProvider';
+
+const HomePage = () => {
+  const [searchValue, setSearchValue] = useState('');
+  const [isFilterOpened, setIsFilterOpened] = useState(false);
+
+  // * Checkbox logic for club
+  const [isClubChecked, setIsClubChecked] = useState(false);
+  const [checkedClubValue, setCheckedClubValue] = useState('');
+
+  // * Checkbox logic for pub
+  const [isPubChecked, setIsPubChecked] = useState(false);
+  const [checkedPubValue, setCheckedPubValue] = useState('');
+
+  // * Checkbox logic for club
+  const [isOpenChecked, setIsOpenChecked] = useState(false);
+  const [checkedOpenValue, setCheckedOpenValue] = useState('');
+
+  // * Reset checkbox logic
+  const resetClubCheckboxHandler = (isChecked) => {
+    if (isChecked === false) {
+      setCheckedClubValue('');
+    }
+  };
+
+  const resetPubCheckboxHandler = (isChecked) => {
+    if (isChecked === false) {
+      setCheckedPubValue('');
+    }
+  };
+
+  const resetOpenCheckboxHandler = (isChecked) => {
+    if (isChecked === false) {
+      setCheckedOpenValue('');
+    }
+  };
+
+  // * Resetting the checkbox name value after it has been de-selected
+  useEffect(() => {
+    resetClubCheckboxHandler(isClubChecked);
+    resetOpenCheckboxHandler(isOpenChecked);
+    resetPubCheckboxHandler(isPubChecked);
+  }, [isClubChecked, isPubChecked, isOpenChecked]);
+
+  return (
+    <>
+      <NavBar
+        onAddSearchTermHandler={setSearchValue}
+        setIsFilterOpened={setIsFilterOpened}
+      />
+      {isFilterOpened && (
+        <Filter
+          setIsPubChecked={setIsPubChecked}
+          setCheckedPubValue={setCheckedPubValue}
+          // ----------------------------------- //
+          setIsClubChecked={setIsClubChecked}
+          setCheckedClubValue={setCheckedClubValue}
+          // ----------------------------------- //
+          setIsOpenChecked={setIsOpenChecked}
+          setCheckedOpenValue={setCheckedOpenValue}
+        />
+      )}
+      <EventContainer
+        searchValue={searchValue}
+        // ------------------------------------ //
+        isClubChecked={isClubChecked}
+        checkedClubValue={checkedClubValue}
+        // ------------------------------------ //
+        isPubChecked={isPubChecked}
+        checkedPubValue={checkedPubValue}
+        // ------------------------------------ //
+        isOpenChecked={isOpenChecked}
+        checkedOpenValue={checkedOpenValue}
+      />
+    </>
+  );
+};
+
+export default HomePage;
