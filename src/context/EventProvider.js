2be21df5199d45bf7bcce03950093b6dea587d51
Ridvan Okovic <okovic.ridvan@gmail.com>
2023-03-13 15:13:08 +0100

    Changed event context

--- src/context/EventProvider.js
+++ src/context/EventProvider.js
@@ -3,21 +3,14 @@ import EventContext from './event-context';
 
 const EventProvider = (props) => {
   const [likedEvents, setLikedEvents] = useState([]);
-  const [isLikedShown, setIsLikedShown] = useState(false);
 
   const addEventsToLiked = (event) => {
     setLikedEvents([event, ...likedEvents]);
   };
 
-  const setIsLikedPanelDisplayed = () => {
-    setIsLikedShown((prev) => !prev);
-  };
-
   const eventContext = {
     events: likedEvents,
-    isLikedPanelShown: isLikedShown,
     addEvent: addEventsToLiked,
-    setIsLikedPanelShown: setIsLikedPanelDisplayed,
   };
 
   return (
