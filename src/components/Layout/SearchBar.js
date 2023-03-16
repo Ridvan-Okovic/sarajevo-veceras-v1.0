969c2712cfb79de417e27851b40bd587e0d78c0a
Ridvan-Okovic <okovic.ridvan@gmail.com>
2023-03-16 21:38:52 +0100

    Changed search bar

--- src/components/Layout/SearchBar.js
+++ src/components/Layout/SearchBar.js
@@ -6,11 +6,11 @@ const SearchBar = (props) => {
   };
 
   return (
-    <div className="group w-full mt-12 flex flex-row items-center justify-center h-10">
+    <div className="w-full px-[10%] mt-[4rem] flex flex-row items-center justify-center h-10">
       <input
         placeholder="Search..."
         onChange={searchChangeHandler}
-        className="focus:border-[#4E10B4] outline-none border-2 border-r-0 rounded-l-lg w-[40%] px-4 py-2 h-full"
+        className="w-[40%] focus:border-[#4E10B4] outline-none border-2 border-r-0 rounded-l-lg px-4 py-2 h-full"
       />
       <button className="h-full px-4 bg-[#4E10B4] rounded-r-lg">
         <FaSearch className="text-xl text-white cursor-pointer" />
