745a9e9141cd066ec9a173999de84e76736dcdb4
Ridvan-Okovic <okovic.ridvan@gmail.com>
2023-04-23 22:23:13 +0200

    Changed checkbox filtering logic

--- src/components/Layout/Filter.js
+++ src/components/Layout/Filter.js
@@ -1,52 +1,38 @@
 const Filter = (props) => {
-  const handleClubCheckboxChange = (event) => {
-    props.setIsClubChecked(event.target.checked);
-    props.setCheckedClubValue(event.target.name);
+  const handleChange = (index) => (e) => {
+    const active = document.getElementById(index).checked;
+    if (active) {
+      props.setSelectedFilter((prev) => [...prev, e.target.value]);
+    } else {
+      props.setSelectedFilter(
+        props.selectedFilter.filter((val) => val !== e.target.value)
+      );
+    }
   };
 
-  const handlePubCheckboxChange = (event) => {
-    props.setIsPubChecked(event.target.checked);
-    props.setCheckedPubValue(event.target.name);
-  };
+  const PLACE_TYPES = ['club', 'pub', 'kafana', 'open air', 'gastro'];
 
-  const handleOpenCheckboxChange = (event) => {
-    props.setIsOpenChecked(event.target.checked);
-    props.setCheckedOpenValue(event.target.name);
-  };
+  const filter = PLACE_TYPES.map((type, i) => {
+    return (
+      <label
+        key={i}
+        className="font-montserrat font-semibold text-lg flex gap-4 text-[#363636]"
+      >
+        {type}
+        <input
+          className="w-4"
+          type="checkbox"
+          id={i}
+          value={type}
+          onChange={handleChange(i)}
+        />
+      </label>
+    );
+  });
 
   return (
     <div className="flex items-center justify-center h-[5rem] shadow-lg bg-[#f6b162]">
-      <div className="flex flex-row gap-10">
-        <label className="font-montserrat font-semibold text-lg flex gap-4 text-[#363636]">
-          Club
-          <input
-            className="w-4"
-            type="checkbox"
-            name="club"
-            onChange={handleClubCheckboxChange}
-          ></input>
-        </label>
-
-        <label className="font-montserrat text-lg font-semibold flex gap-4 text-[#363636]">
-          Pub
-          <input
-            className="w-4"
-            type="checkbox"
-            name="pub"
-            onChange={handlePubCheckboxChange}
-          ></input>
-        </label>
-
-        <label className="font-montserrat text-lg font-semibold flex gap-4 text-[#363636]">
-          Open Air
-          <input
-            className="w-4"
-            type="checkbox"
-            name="open air"
-            onChange={handleOpenCheckboxChange}
-          ></input>
-        </label>
-      </div>
+      <div className="flex flex-row gap-10">{filter}</div>
     </div>
   );
 };
