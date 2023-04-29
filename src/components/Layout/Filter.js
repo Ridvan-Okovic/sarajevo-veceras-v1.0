e83f0b8ae955e2311af2aaf381a75fbff9d70535
Ridvan-Okovic <okovic.ridvan@gmail.com>
2023-04-29 22:57:06 +0200

    Added dropdown menu in the filter component

--- src/components/Layout/Filter.js
+++ src/components/Layout/Filter.js
@@ -1,4 +1,10 @@
+import { useState } from 'react';
+
+import { MdArrowDropDown, MdArrowDropUp } from 'react-icons/md';
+
 const Filter = (props) => {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   const handleChange = (index) => (e) => {
     const active = document.getElementById(index).checked;
     if (active) {
@@ -11,6 +17,16 @@ const Filter = (props) => {
   };
 
   const PLACE_TYPES = ['Club', 'Pub', 'Kafana', 'Open Air', 'Gastro'];
+
+  const DAYS = [
+    'Nedelja',
+    'Ponedeljak',
+    'Utorak',
+    'Srijeda',
+    'Četvrtak',
+    'Petak',
+    'Subota',
+  ];
 
   const filter = PLACE_TYPES.map((type, i) => {
     return (
@@ -31,8 +47,37 @@ const Filter = (props) => {
   });
 
   return (
-    <div className="flex items-center justify-center shadow-lg bg-zinc-900 px-12 py-4 rounded-lg">
+    <div className="flex items-center justify-center shadow-lg bg-zinc-900 px-12 py-4 rounded-lg gap-10">
       <div className="flex flex-row gap-10">{filter}</div>
+      <div className="relative">
+        <button
+          onClick={() => setIsDropdownOpen((prev) => !prev)}
+          className="flex flex-row items-center justify-between w-32 h-8 shadow-2xl px-3 rounded-md bg-zinc-800 opacity-80 hover:opacity-100 active:opacity-100"
+        >
+          <p className="text-[#e1e1e1]">Days</p>
+          {!isDropdownOpen ? (
+            <MdArrowDropDown className="text-[#e1e1e1] text-2xl" />
+          ) : (
+            <MdArrowDropUp className="text-[#e1e1e1] text-2xl" />
+          )}
+        </button>
+        {isDropdownOpen && (
+          <div className="absolute top-10 rounded-md w-32 bg-zinc-800 shadow-lg left-0 border-dashed px-2 py-2">
+            <ul>
+              {DAYS.map((day, index) => {
+                return (
+                  <li
+                    className=" text-[#e1e1e1] cursor-pointer opacity-80 hover:opacity-100 active:opacity-100"
+                    key={index}
+                  >
+                    {day}
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
