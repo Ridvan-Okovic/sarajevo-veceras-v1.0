26b96666163e5ba4c98f1a625643f7c0a5d5d83d
Ridvan-Okovic <okovic.ridvan@gmail.com>
2023-05-04 19:20:35 +0200

    Added state persisting on closing the dropdown

--- src/components/Layout/Filter.js
+++ src/components/Layout/Filter.js
@@ -15,6 +15,8 @@ const Filter = (props) => {
     'Subota',
   ];
 
+  const PLACE_TYPES = ['Club', 'Pub', 'Kafana', 'Open Air', 'Gastro'];
+
   const handleChangeType = (index) => (e) => {
     const active = document.getElementById(index).checked;
     if (active) {
@@ -36,8 +38,6 @@ const Filter = (props) => {
       );
     }
   };
-
-  const PLACE_TYPES = ['Club', 'Pub', 'Kafana', 'Open Air', 'Gastro'];
 
   const filter = PLACE_TYPES.map((type, i) => {
     return (
@@ -71,6 +71,7 @@ const Filter = (props) => {
           type="checkbox"
           className=" peer-hover:opacity-100 opacity-80 appearance-none w-5 h-5 relative rounded-sm border cursor-pointer focus:outline-none transition-all duration-300 checked:bg-[#ffb560] checked:border-[#ffb560] after:content-[''] after:absolute after:w-full after:h-full after:bg-no-repeat after:bg-center after:bg-[length:15px] after:checked:bg-[url('https://www.svgrepo.com/show/105291/check-mark.svg')]"
           onChange={handleChangeDay(day)}
+          checked={props.selectedDayFilter.includes(day)}
         />
       </label>
     );
