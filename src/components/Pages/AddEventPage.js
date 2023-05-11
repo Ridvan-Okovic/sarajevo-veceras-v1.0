1e81be413e2a535d0c63a304abf832dcea760a54
Ridvan-Okovic <okovic.ridvan@gmail.com>
2023-05-11 18:54:20 +0200

    Added event adding form

--- src/components/Pages/AddEventPage.js
+++ src/components/Pages/AddEventPage.js
@@ -2,25 +2,62 @@ import { motion } from 'framer-motion';
 
 const AddEventPage = () => {
   return (
-    <div className="w-full grid place-items-center mt-8 text-[#e1e1e1]">
-      <div className="w-[50%] bg-zinc-900 border-zinc-800">
-        <form className="flex flex-col space-y-2 w-full px-4 py-6 ">
-          <label>Lokacija</label>
-          <input className="bg-zinc-800 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg" />
-          <label>Opis</label>
-          <input className="bg-zinc-800 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg" />
-          <label>Adresa</label>
-          <input className="bg-zinc-800 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg" />
-          <label>Vrijeme</label>
-          <input className="bg-zinc-800 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg" />
-          <label>Datum</label>
-          <input className="bg-zinc-800 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg" />
-          <label>Tip</label>
-          <input className="bg-zinc-800 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg" />
-          <label>Slika</label>
-          <input className="bg-zinc-800 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg" />
+    <div className="w-full grid place-items-center mt-16 text-[#e1e1e1]">
+      <div className="w-[32rem] bg-zinc-900 border-zinc-800 shadow-lg rounded-lg px-4">
+        <form className="flex flex-col space-y-4 w-full px-4 py-6 ">
+          <div className="flex flex-col gap-1">
+            <label className="text-xl">Lokacija</label>
+            <input
+              placeholder="Sloga, Dorian Gray..."
+              className="bg-zinc-800 px-2 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg"
+            />
+          </div>
+          <div className="flex flex-col">
+            <label className="text-xl">Opis</label>
+            <input
+              placeholder="DJ Nimra..."
+              className="bg-zinc-800 px-2 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label className="text-xl">Adresa</label>
+            <input
+              placeholder="Mehmeda Spahe, 20"
+              className="bg-zinc-800 px-2 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label className="text-xl">Vrijeme</label>
+            <input
+              type="time"
+              className="bg-zinc-800 px-2 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg"
+            />
+          </div>
+
+          <div className="flex flex-col gap-1">
+            <label className="text-xl">Datum</label>
+            <input
+              type="date"
+              className="bg-zinc-800 px-2 placeholder:text-[#e1e1e1] text-[#e1e1e1] rounded border border-opacity-50 border-gray-300 h-8 shadow-lg"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label className="text-xl">Tip</label>
+            <input
+              placeholder="Pub, club..."
+              className="bg-zinc-800 px-2 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label className="text-xl">Slika</label>
+            <input
+              placeholder="Link slike (https://...)"
+              className="bg-zinc-800 px-2 rounded border border-opacity-50 border-gray-300 h-8 shadow-lg"
+            />
+          </div>
+
           <div className="w-full flex items-center justify-center">
-            <motion.button className="w-24 bg-zinc-900 shadow-md rounded-lg py-1">
+            <motion.button className="w-24 bg-zinc-800 mt-8 shadow-md rounded-lg py-1">
               Submit
             </motion.button>
           </div>
